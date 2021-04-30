547ed7aba8896f534df8ff104ec945e32824a9c0
kirankraleti <kiran.kraleti@bizcloudexperts.com>
2021-04-29 22:29:22 -0500

    useRef hook

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
-import React, { useState, useCallback } from 'react';
-import Title from './components/title';
-import Count from './components/count';
-import CountButton from './components/countButton';
-import Age from './components/age';
-import AgeButton from './components/ageButton';
+// This is how it is done with hooks.
+import React, {useRef} from 'react';
+
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-  const [age, setAge] = useState(10);
+  const textInput = useRef();
 
-  // if we use like this, the count button never gets re-rendered as it is not using any outside properties
-  const incrementCount = useCallback(() => {
-    setCount(prevCount => prevCount+1);
-  }, []);
+  const triggerHandler = () => {
+    console.log(textInput.current.value);
+  }
 
-  // if we use like this, the count button gets re-rendered as it is using outside properties like age
-  const incrementAge = useCallback(() => {
-    setAge(age+1);
-  }, [age]);
-
-  return (
-    <>
-      <Title/>
-      <Count count={count}/>
-      <CountButton handleCount={incrementCount}/>
-      <Age age={age}/>
-      <AgeButton handleAge={incrementAge}/>
-    </>
-  );
+    return(
+      <>
+      <h1>Form:</h1>
+      {/* <input type="text" ref={textInput}/> */}
+      <InputComponent ref={textInput}/>
+      <button onClick={triggerHandler}>
+        Trigger
+      </button>
+      </>
+    )
 }
+
+// Ref's cannot be passed as props
+// So below wont work
+
+// const InputComponent = ({ref}) => {
+//   return (
+//     <input type="text" ref={ref}/>
+//   )
+// }
+
+// Solution
+
+const InputComponent = React.forwardRef((props, ref) => {
+  return (
+    <input type="text" ref={ref}/>
+  )
+});
 
 export default App;
